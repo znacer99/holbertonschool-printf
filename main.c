41d82d324752411e86bdc61901c5ba212902e271
root <6274@holbertonstudents.com>
2023-04-23 17:56:43 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+int count = 0;
+count = _printf("Hello, world!\n");
+printf("Number of characters printed: %d\n", count);
+return (0);
+}
